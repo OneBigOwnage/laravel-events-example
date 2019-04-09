01533bf744e30a460f9678e1695bb1e24cf8c3bc
OneBigOwnage <niekvandenbos@hotmail.com>
2019-04-09 20:28:49 +0200

    Fill in the events/listeners mapping.

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,9 +2,10 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Event;
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
+use App\Events\ActivateMemo;
+use App\Listeners\SetMemoActive;
+use App\Listeners\NotifyFleetOwner;
+
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
@@ -15,20 +16,10 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
-        ],
+        ActivateMemo::class => [
+            SetMemoActive::class   ,
+            NotifyFleetOwner::class,
+            DistributeMemo::class  ,
+        ]
     ];
-
-    /**
-     * Register any events for your application.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        parent::boot();
-
-        //
-    }
 }
