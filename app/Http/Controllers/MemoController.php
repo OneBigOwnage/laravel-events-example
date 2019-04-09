b2ea90e65561c96f9f7cd66279e65ca5ec86fa59
OneBigOwnage <niekvandenbos@hotmail.com>
2019-04-09 21:34:03 +0200

    Create a controller, to wire everything up.

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -2,9 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Memo;
+use App\Events\ActivateMemo;
 use Illuminate\Http\Request;
 
 class MemoController extends Controller
 {
-    //
+    /**
+     * Show the default view.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index()
+    {
+        return view('default');
+    }
+
+    /**
+     * Create & distribute a memo.
+     *
+     * @param  Request $request The incoming request.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function activateMemo(Request $request)
+    {
+        $request->validate([ 'title' => 'required|string', 'description' => 'required|string' ]);
+
+        $title       = $request->title;
+        $description = $request->description;
+
+        $memo = new Memo($title, $description);
+
+
+        $event = new ActivateMemo($memo);
+
+        event($event);
+
+
+        return view('default');
+    }
 }
